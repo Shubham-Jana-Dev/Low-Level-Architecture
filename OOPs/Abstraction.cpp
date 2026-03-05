bebfe03bd13659172af6a220f0865aa2f6611f64
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-03-05 05:53:12 +0530

    feat: understood abstraction and implementation

---
+++ OOPs/Abstraction.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+class AbstractEmployee{
+public:
+    virtual void AskForPromotion()=0;
+};
+class Employee: public AbstractEmployee{
+private:
+    string Name;
+    string Company;
+   int Age;
+public:
+   void setName (string name){
+    Name = name;
+   }
+   string getName (){
+        return Name;
+   }
+   void setCompany (string company){
+        Company = company;
+   }
+   string getCompany(){
+        return Company;
+   }
+   void setAge(int age){
+        if(age>=18){
+        Age = age;
+        }
+        else{
+            Age = -1;
+            cout << "Plese enter a valid age " << Name << endl;
+        }
+   }
+   int getAge(){
+        return Age;
+   }
+   Employee(string name, string company, int age){
+    Name = name;
+    Company = company;
+    setAge(age);
+   }
+   void AskForPromotion(){
+        if (Age>30){
+            cout << Name << " got promoted! :)" << endl;
+        }
+        else{
+            cout << Name << " better luck next time. :(" << endl;
+        }
+   }
+};
+int main(){
+    Employee emp1 = Employee("Shubham","Wipro",29);
+    emp1.AskForPromotion();
+    return 0;
+}
